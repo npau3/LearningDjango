a88c7efc0230e9d66df9a879a6e9df01ae76ca08
npau3 <68152279+npau3@users.noreply.github.com>
2020-07-21 17:01:24 +0300

    asd

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('create', views.create, name='create'),
     path('update_page', views.update_page, name='update_page')
