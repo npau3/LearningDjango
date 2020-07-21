0d7af8144140a4fe834c463f0956d5e57ea7271f
npau3 <68152279+npau3@users.noreply.github.com>
2020-07-21 16:51:21 +0300

    12313

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    tasks = Task.objects.order_by('id')
+    tasks = Task.objects.all().order_by('id')
     return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 
@@ -13,19 +14,28 @@ def about(request):
 
 
 def create(request):
+    success = False
     error = ''
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
+            success = True
             form.save()
             return redirect('create')
         else:
             error = 'Форма была неверной'
     form = TaskForm()
     context = {
-
         'form': form,
-        'error': error
+        'error': error,
+        'success': success
+    }
+    return render(request, 'main/create.html', context)
 
+
+def update_page(request):
+    form = TaskForm()
+    context = {
+        'form': form
     }
     return render(request, 'main/create.html', context)
