1297cd028549396bda31b27a9c1beb189db4575d
npau3 <prize890@gmail.com>
2021-08-17 23:39:04 +0300

    urls py, requirements.txt

--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include('main.urls')),
-    path('', include('accounts.urls'))
-]
+    path('', include('accounts.urls')),
+    path('api/', include('api.urls'))
+    ]
